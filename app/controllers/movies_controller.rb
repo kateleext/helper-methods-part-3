9fe06ea745b4107a612145da451158ba2bf650ec
kateleext <83829378+kateleext@users.noreply.github.com>
2025-04-03 02:02:13 +0000

    devise

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
 
   def index
     @movies = Movie.order(created_at: :desc)
-
     respond_to do |format|
       format.json { render json: @movies }
 
@@ -14,14 +15,10 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    movie_params = params.require(:movie).permit(:title, :description)
-
     @movie = Movie.new(movie_params)
-
     if @movie.valid?
       @movie.save
 
@@ -32,14 +29,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-
-    movie_params = params.require(:movie).permit(:title, :description)
-
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie was successfully updated."
     else
@@ -48,10 +40,18 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
-
     @movie.destroy
 
     redirect_to movies_url, notice: "Movie was successfully destroyed."
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 end
